41712ec1a11216c95df13d95fa30fe18eda6b9a5
temfa <akinfetemitope5@gmail.com>
2023-11-03 15:38:48 +0100

    Done

--- app/Admin/Deleted/page.tsx
+++ app/Admin/Deleted/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Title from "@/component/Layout/Title";
-import { deleted } from "@/utils/data";
-import SingleReportCard from "@/component/SmallComponents/singleReportCard";
+// import { deleted } from "@/utils/data";
+// import SingleReportCard from "@/component/SmallComponents/singleReportCard";
 import DashboardLayout from "@/component/layouts/dashboard-layout";
 
 const Deleted = () => {
@@ -10,11 +10,11 @@ const Deleted = () => {
     <DashboardLayout>
       <div className={styles.draftContainer}>
         <Title text="Deleted Reports" />
-        <div className={styles.draftWrapper}>
+        {/* <div className={styles.draftWrapper}>
           {deleted?.map((item, index) => {
             return <SingleReportCard title={item.title} text={item.action} number={item.number} key={index} type={false} />;
           })}
-        </div>
+        </div> */}
       </div>
     </DashboardLayout>
   );
--- app/Admin/Draft/page.tsx
+++ app/Admin/Draft/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Title from "@/component/Layout/Title";
-import { draft } from "@/utils/data";
-import SingleReportCard from "@/component/SmallComponents/singleReportCard";
+// import { draft } from "@/utils/data";
+// import SingleReportCard from "@/component/SmallComponents/singleReportCard";
 import DashboardLayout from "@/component/layouts/dashboard-layout";
 
 const Draft = () => {
@@ -11,9 +11,9 @@ const Draft = () => {
       <div className={styles.draftContainer}>
         <Title text="Reports Drafts" />
         <div className={styles.draftWrapper}>
-          {draft?.map((item, index) => {
+          {/* {draft?.map((item, index) => {
             return <SingleReportCard title={item.title} text={item.action} number={item.number} key={index} type={false} />;
-          })}
+          })} */}
         </div>
       </div>
     </DashboardLayout>
